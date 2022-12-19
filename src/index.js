c7c7b58360ab1a227fddcba4dbdddfcd2fc95d7e
AntonySawyer <a.krutenok@gmail.com>
2022-12-19 09:18:17 +0300

    refactor: Update initApp script

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import * as readline from 'readline/promises';
 import { FM_COMMANDS } from './constants/command.js';
 
-import { CLI_ARGUMENT_NAME } from './constants/main.js';
 import { initApp } from './modules/generalModule.js';
-import { parseCliArgument } from './utils/arguments.js';
 import { resolveCommand } from './utils/commands.js';
-import { greetWithUser, sayByeToUser, sayWorkingDirectory } from './utils/messages.js';
+import { sayByeToUser, sayWorkingDirectory } from './utils/messages.js';
 
 
-initApp();
-
-const userName = parseCliArgument(CLI_ARGUMENT_NAME.USERNAME);
-
-greetWithUser(userName);
-sayWorkingDirectory();
+const userName = initApp();
 
 const readlineInterface = readline.createInterface(process.stdin, process.stdout);
 
